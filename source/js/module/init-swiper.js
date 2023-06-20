6562930d4a9fafe7ad6cc4d69332a840f759e309
OlgaNikl <hi.olganikl@gmail.com>
2023-06-20 18:16:10 +0300

    добавляет графику

---
+++ source/js/module/init-swiper.js
@@ -0,0 +1,15 @@
+// const initSwiper = () => {
+//   const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+
+//     navigation: {
+//       nextEl: '.coaches__button--next',
+//       prevEl: '.coaches__button--prev',
+//     },
+//   });
+
+//   return swiper;
+// };
+
+// export { initSwiper };
--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,24 @@
 /* в этот файл добавляет скрипты*/
-L.map('map')
-  .setView({
-    lat: 59.92749,
-    lng: 30.31127,
-  }, 10);
+// import { initSwiper } from "./module/init-swiper";
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// initSwiper();
 
-const marker = L.marker(
-  {
-    lat: 59.96831,
-    lng: 30.31748,
-  },
-);
+// L.map('map')
+//   .setView({
+//     lat: 59.92749,
+//     lng: 30.31127,
+//   }, 10);
 
-marker.addTo(map);
+//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+// const marker = L.marker(
+//   {
+//     lat: 59.96831,
+//     lng: 30.31748,
+//   },
+// );
+
+// marker.addTo(map);
