6a7378011b9c4dac9723bc174b3af7c810d073e0
OlgaNikl <hi.olganikl@gmail.com>
2023-06-26 17:16:00 +0300

    подключает карту, добавляет интерактив состояния

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,56 @@
 /* в этот файл добавляет скрипты*/
-// import { initSwiper } from "./module/init-swiper";
 
-// initSwiper();
+/* Слайдер */
 
-// L.map('map')
-//   .setView({
-//     lat: 59.92749,
-//     lng: 30.31127,
-//   }, 10);
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
 
-//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: true,
+});
 
-// const marker = L.marker(
-//   {
-//     lat: 59.96831,
-//     lng: 30.31748,
-//   },
-// );
+/* Карта */
 
-// marker.addTo(map);
+const map = L.map('map')
+  .setView({
+    lat: 59.96831,
+    lng: 30.31748,
+  }, 18);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const markerIcon = L.icon({
+  iconURL: '/../img/marker-icon.svg',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const markerOptions = {
+  icon: markerIcon,
+};
+
+// L.marker([59.96831, 30.31748], {icon: markerIcon}).addTo(map);
+
+const marker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+);
+
+marker.addTo(map);
+
+// marker.on('moveend', (evt) => {
+//   console.log(evt.target.getLatLng());
+// });
